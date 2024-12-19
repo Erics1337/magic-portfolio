f61293f4eeaa3e7c4306d891496389dcc09ebb18
Eric <erics1337@gmail.com>
2024-12-19 11:52:39 -0700

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-    experimental: {
-        workerThreads: false, // Disable worker threads if applicable
-        outputFileTracing: true,
-      },
-    output: "standalone", // Use the standalone build for Next.js
     webpack: (config) => {
         config.resolve.alias = {
             ...config.resolve.alias,
