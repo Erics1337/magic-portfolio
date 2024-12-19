536f8a4e3e0c6b6169132003d2719010bc26b532
Eric <erics1337@gmail.com>
2024-12-19 04:56:31 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = {
+    output: 'export',  // Enable static exports
+    distDir: 'out',   // Specify the output directory
     images: {
         unoptimized: true,
         remotePatterns: [
